21fc429a06ac809310fef4a304fc2865dd48ee02
Leonardo Vinicius Kaminski Ferreira <leonardo@mindvalley.com>
2017-09-06 19:32:50 +0800

    Added logic to app

--- ColorMakeSliders/ColorMakeSliders/ViewController.swift
+++ ColorMakeSliders/ColorMakeSliders/ViewController.swift
@@ -9,6 +9,25 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    enum Color: Int {
+        case red = 0
+        case green
+        case blue
+    }
+    
+    struct RGB {
+        var redColor: CGFloat = 0.0
+        var greenColor: CGFloat = 0.0
+        var blueColor: CGFloat = 0.0
+    }
+    
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    @IBOutlet weak var colorView: UIView!
+    
+    var rgbColors = RGB()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +39,25 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    // MARK: - Actions
+    
+    @IBAction func sliderChanged(_ sender: UISlider) {
+        
+        switch Color(rawValue: sender.tag)! {
+        case .red:
+            rgbColors.redColor = CGFloat(redSlider.value)
+            break
+        case .green:
+            rgbColors.greenColor = CGFloat(greenSlider.value)
+            break
+        case .blue:
+            rgbColors.blueColor = CGFloat(blueSlider.value)
+            break
+        }
+        
+        colorView.backgroundColor = UIColor(red: rgbColors.redColor, green: rgbColors.greenColor, blue: rgbColors.blueColor, alpha: 1.0)
+    }
+    
 
 }
 
